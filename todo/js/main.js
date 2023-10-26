5d59f324af8b79bec22a033c89b79c8427515a26
lucho-acdc <lucianohamoroso@gmail.com>
2023-10-26 10:07:22 -0300

    primera subida

---
+++ todo/js/main.js
@@ -0,0 +1,224 @@
+(function() {
+  "use strict";
+
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
+  /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all)
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach(e => e.addEventListener(type, listener))
+      } else {
+        selectEl.addEventListener(type, listener)
+      }
+    }
+  }
+
+  /**
+   * Easy on scroll event listener 
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }
+
+  /**
+   * Navbar links active state on scroll
+   */
+  let navbarlinks = select('#navbar .scrollto', true)
+  const navbarlinksActive = () => {
+    let position = window.scrollY + 200
+    navbarlinks.forEach(navbarlink => {
+      if (!navbarlink.hash) return
+      let section = select(navbarlink.hash)
+      if (!section) return
+      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+        navbarlink.classList.add('active')
+      } else {
+        navbarlink.classList.remove('active')
+      }
+    })
+  }
+  window.addEventListener('load', navbarlinksActive)
+  onscroll(document, navbarlinksActive)
+
+  /**
+   * Scrolls to an element with header offset
+   */
+  const scrollto = (el) => {
+    let elementPos = select(el).offsetTop
+    window.scrollTo({
+      top: elementPos,
+      behavior: 'smooth'
+    })
+  }
+
+  /**
+   * Back to top button
+   */
+  let backtotop = select('.back-to-top')
+  if (backtotop) {
+    const toggleBacktotop = () => {
+      if (window.scrollY > 100) {
+        backtotop.classList.add('active')
+      } else {
+        backtotop.classList.remove('active')
+      }
+    }
+    window.addEventListener('load', toggleBacktotop)
+    onscroll(document, toggleBacktotop)
+  }
+
+  /**
+   * Mobile nav toggle
+   */
+  on('click', '.mobile-nav-toggle', function(e) {
+    select('body').classList.toggle('mobile-nav-active')
+    this.classList.toggle('bi-list')
+    this.classList.toggle('bi-x')
+  })
+
+  /**
+   * Scrool with ofset on links with a class name .scrollto
+   */
+  on('click', '.scrollto', function(e) {
+    if (select(this.hash)) {
+      e.preventDefault()
+
+      let body = select('body')
+      if (body.classList.contains('mobile-nav-active')) {
+        body.classList.remove('mobile-nav-active')
+        let navbarToggle = select('.mobile-nav-toggle')
+        navbarToggle.classList.toggle('bi-list')
+        navbarToggle.classList.toggle('bi-x')
+      }
+      scrollto(this.hash)
+    }
+  }, true)
+
+  /**
+   * Scroll with ofset on page load with hash links in the url
+   */
+  window.addEventListener('load', () => {
+    if (window.location.hash) {
+      if (select(window.location.hash)) {
+        scrollto(window.location.hash)
+      }
+    }
+  });
+
+  /**
+   * Hero type effect
+   */
+const typedElement = document.querySelector('.typed');
+if (typedElement) {
+  new Typed(typedElement, {
+    strings: ["TRATAMIENTOS DE SUPERFICIES METALLICAS", "DESDE 1986"],
+    typeSpeed: 100,
+    backSpeed: 70,
+    backDelay: 2000,
+    loop: true,
+    showCursor: false,
+  });
+}
+
+
+
+  /**
+   * Porfolio isotope and filter
+   */
+ // Espera a que se cargue la página
+window.addEventListener('load', () => {
+  // Encuentra el contenedor de cartera (portfolio)
+  const portfolioContainer = document.querySelector('.portfolio-container');
+
+  if (portfolioContainer) {
+    // Inicializa Isotope en el contenedor de cartera
+    const portfolioIsotope = new Isotope(portfolioContainer, {
+      itemSelector: '.portfolio-item'
+    });
+
+    // Encuentra y escucha los elementos de filtro
+    const portfolioFilters = document.querySelectorAll('#portfolio-flters li');
+
+    portfolioFilters.forEach(filter => {
+      // Agrega un evento de clic a cada elemento de filtro
+      filter.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        // Elimina la clase 'filter-active' de todos los elementos de filtro
+        portfolioFilters.forEach(el => {
+          el.classList.remove('filter-active');
+        });
+
+        // Agrega la clase 'filter-active' al elemento de filtro seleccionado
+        filter.classList.add('filter-active');
+
+        // Filtra los elementos de la cartera según la opción seleccionada
+        portfolioIsotope.arrange({
+          filter: filter.getAttribute('data-filter')
+        });
+
+        // Refresca las animaciones en función del desplazamiento
+        AOS.refresh();
+      });
+    });
+  }
+});
+
+
+  /**
+   * Initiate portfolio lightbox 
+   */
+  const portfolioLightbox = GLightbox({
+    selector: '.portfolio-lightbox'
+  });
+
+  /**
+   * Portfolio details slider
+   */
+  new Swiper('.portfolio-details-slider', {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  });
+
+
+  /**
+   * Animation on scroll
+   */
+  window.addEventListener('load', () => {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    })
+  });
+
+  /**
+   * Initiate Pure Counter 
+   */
+  new PureCounter();
+
+})()
